7b61cff88def0290b4fea1855cf05ac3c27f62dc
pinto0309 <rmsdh122@yahoo.co.jp>
2022-04-04 00:49:41 +0900

    init

---
+++ setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="scs4onnx",
+    version="0.0.1",
+    description="A very simple tool that compresses the overall size of the ONNX model by aggregating duplicate constant values as much as possible. Simple Constant value Shrink for ONNX.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Katsuya Hyodo",
+    author_email="rmsdh122@yahoo.co.jp",
+    url="https://github.com/PINTO0309/scs4onnx",
+    license="MIT License",
+    packages=find_packages(),
+    platforms=["linux", "unix"],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "scs4onnx = scs4onnx.onnx_shrink_constant:main"
+        ]
+    }
+)
